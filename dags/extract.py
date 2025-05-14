ecb397ad5294328db9f222316987e2f2fffe4a69
MatheusDiogo <matheusdiogoeng@gmail.com>
2025-05-13 23:16:28 -0300

    Problema de memória resolvido. Extração e carregamento de dados brutos em banco local validado.

--- dags/extract.py
+++ dags/extract.py
@@ -30,8 +30,8 @@ def extrair_dados():
                     colunas_com_tipos_mistos = [21,22,44,45,46,50,52,54,56,62,74,85,101]
                     dtype_dict = {col: 'str' for col in colunas_com_tipos_mistos}
 
-                    df = pd.read_csv(csv_file, sep=',', encoding='latin1', dtype=dtype_dict)
-                    df_pe = df[df['SG_UF_NOT'] == 26]
+                    chunks = pd.read_csv(csv_file, sep=',', encoding='latin1', dtype=dtype_dict, chunksize=50000)
+                    df_pe = pd.concat([chunk[chunk['SG_UF_NOT'] == 26] for chunk in chunks])
 
             # Cria uma conexão com o DuckDB e cria a tabela
             conn = duckdb.connect('/opt/airflow/output/dados_sinan.db')
@@ -40,8 +40,13 @@ def extrair_dados():
             # Registra o DataFrame como uma tabela temporária (view)
             conn.register('df_pe', df_pe)
 
-            # Substitui ou cria a tabela 'dengue.pe'
-            conn.execute("CREATE TABLE IF NOT EXISTS dengue.dengue_pe AS SELECT * FROM df_pe")
+            # Substitui a tabela se já existir
+            conn.execute("DROP TABLE IF EXISTS dengue.dengue_pe")
+
+            # Cria a tabela com os dados do DataFrame
+            conn.execute("CREATE TABLE dengue.dengue_pe AS SELECT * FROM df_pe")
+
+            print(conn.sql("SELECT * FROM dengue.dengue_pe").to_df().head()) # Validar extração
 
             # Fecha a conexão
             conn.close()
@@ -49,6 +54,14 @@ def extrair_dados():
         else:
             raise Exception(f"Erro ao acessar API: {response.status_code}")
 
+def transformar_dados():
+    # Cria uma conexão com o DuckDB e cria a tabela
+    conn = duckdb.connect('/opt/airflow/output/dados_sinan.db')
+
+    print(conn.sql("SELECT * FROM dengue.dengue_pe").to_df().head())
+
+    # Fecha a conexão
+    conn.close()
 
 default_args = {
     "owner": "airflow",
@@ -71,5 +84,11 @@ task_checar_datasus = PythonOperator(
     dag=dag
 )
 
+task_transformar_dados = PythonOperator(
+    task_id="transformar_dados",
+    python_callable=transformar_dados,
+    dag=dag
+)
+
 # Definindo a ordem de execução
-task_checar_datasus
+task_checar_datasus >> task_transformar_dados
